3d2f5c410f898cf4d06acb2373b4dce3cf804fe7
Luis Felipe <luisFAOSetecH@gmail.com>
2020-12-01 06:58:02 -0300

    splitting forms

--- mobile/src/screens/registerBO/registerBO.js
+++ mobile/src/screens/registerBO/registerBO.js
@@ -1,28 +1,116 @@
-import { Entypo, Ionicons, MaterialIcons } from '@expo/vector-icons';
-import React from 'react';
-import { Keyboard, KeyboardAvoidingView, Text, View } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import AppInputBox from '../../components/AppInputBox/AppInputBox';
+import { Ionicons } from '@expo/vector-icons';
+import React, { useContext, useState } from 'react';
+import { ActivityIndicator, Image, Keyboard, KeyboardAvoidingView, Platform, Text, TouchableWithoutFeedback, View } from 'react-native';
 import Header from '../../components/Header/Header';
+import * as DocumentPicker from 'expo-document-picker';
+import { EvaIconsPack } from '@ui-kitten/eva-icons'
+import { IconRegistry, IndexPath } from '@ui-kitten/components'
+import * as Location from 'expo-location';
+import moment from 'moment'
+import baseURL from '../../baseURL';
+
 
 import { 
   Container,
-  InputContainer, 
-  HeaderInputContainer,
-  TitleInputContainer,
   PageTitle,
- } from './styles';
+  WrapperSuccessImage,
+  SuccessImage,
+  SuccessText
+} from './styles';
 
-function registerBO({navigation}) {
+import SecondPage from './SecondePage/SecondPage';
+import FirstPage from './FirstPage/FirstPage';
+import AuthContext from '../../context/context';
+import ConfirmModal from '../../components/ConfirmModal/ConfirmModal';
+import { useEffect } from 'react';
+import HideKeyboardContext from '../../context/hideTabContext';
 
-  const [BOdatas, setBOdatas] = React.useState({
-    type:['Roubo ou Furto de Veículos','Desaparecimento de Pessoas', 'Roubo de objetos pessoais', 'Furto de itens públicos'],
-    localIdentification: '',
-    description: '',
-    dateOccurred: [],
+function RegisterBO({navigation}) {
 
+  const [loading, setLoading] = useState(false)
+  const [loadingButton, setLoadingButton] = useState(false)
+
+  const selectInputData = [
+    'Roubo ou Furto de Veículos',
+    'Desaparecimento de Pessoas',
+    'Roubo ou Furto de objetos pessoais',
+    'Roubo ou Furto de itens públicos',
+    'Fuga de presidiários',
+  ]
+
+  const getLastCreatedBy = async ()=> {
+    setLoading(true)
+    try {
+
+      const response = await fetch(baseURL+'record/lastCreatedAt', {
+        method: 'GET',
+        headers:{authorization: authDatas.token},
+      })
+
+      let lastCreatedAt= await response.text()
+
+      if(!lastCreatedAt) throw new Error('O usuário pode inserir')
+
+      lastCreatedAt= new Date(lastCreatedAt)
+
+      setIsSuccess(!moment(lastCreatedAt).fromNow().includes('days ago'))
+
+    } catch (error) {
+      console.log(error)
+      setIsSuccess(false)
+    }
+    setLoading(false)
+  }
+
+  useEffect(()=> {
+    getLastCreatedBy()
+  },[])
+
+  const [BOdatas, setBOdatas] = useState({
+    tipo_registro: new IndexPath(0),
+    endereco_cep: '',
+    desc_registro: '',
+    data_ocorrido: new Date(),
   })
 
+  const [boletimFile, setBoletimFile] = useState(null)
+
+  const {authDatas} = useContext(AuthContext)
+
+  const [locationDatas, setLocationDatas] = useState({})
+
+  const [nextPage, setNextPage] = useState(1)
+
+  const [isSuccess, setIsSuccess] = useState(false)
+ 
+  const selectPDF = async() => {
+    try {
+      let {name, size, uri, type} = await DocumentPicker.getDocumentAsync({});
+
+      if(type === "success") {
+
+        const allowedTypes = ['.jpeg','.jpg', '.png', '.pdf']
+
+        let isVerified = 0
+        
+        if(size >= 1572864) throw new Error('Arquivo muito grande: máx: 1.5MB')
+
+        allowedTypes.forEach(type => {
+            if(name.substring(name.lastIndexOf('.')) === type) return
+            else isVerified++
+        });
+
+        if(isVerified == allowedTypes.length) throw new Error('Esse tipo não é aceito -> '+name.substring(name.lastIndexOf('.')))
+      }
+      
+      else throw new Error('Você cancelou o upload')
+
+      setBoletimFile({name, size, uri})
+
+    } catch (error) {
+      setBoletimFile(error.message)
+    }
+  }
 
   const StateValueHandler = (field, value) => {
 
@@ -30,75 +118,154 @@ function registerBO({navigation}) {
 
     lastState[field] = value
 
-
     setBOdatas({...lastState})
   }
 
-  return (
-      <Container>
-        <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
-          <Header 
-            navigation={navigation} 
-            page="Verificar Segurança"/>
-          <KeyboardAvoidingView behavior='position'>
-            <PageTitle>
-              <Text 
-                style={{
-                  fontFamily:'RobotoLight', 
-                  fontSize:35, 
-                  textAlign: 'center'}}>
-                INSIRA OS DADOS SOBRE O BOLETIM
-              </Text>
-              <Ionicons name="ios-arrow-down" size={30} color="black" />
-            </PageTitle>
-            <InputContainer>
-              <HeaderInputContainer>
-                <Entypo name="location" size={25} color="black" />
-                <TitleInputContainer>Local do ocorrido</TitleInputContainer>
-              </HeaderInputContainer>
-              <AppInputBox
-                  onChangeText={localIdentification => StateValueHandler("localIdentification", localIdentification)}
-                  value={BOdatas.localIdentification}
+  const NextPage = async (stateErrorFunction) => {
+    setLoadingButton(true)
+
+    try {
+      console.log(BOdatas.endereco_cep)
+      const GeocodeResponse = await Location.geocodeAsync(BOdatas.endereco_cep)
+
+      const {latitude, longitude} = GeocodeResponse[0]
+
+      setLocationDatas({latitude, longitude})
+      
+      setNextPage(2)
+      stateErrorFunction(false)
+    } catch (error) {
+      stateErrorFunction(true)
+      console.log('erroooooo',error)
+    }
+    setLoadingButton(false)
+  }
+
+
+
+  const SubmitDatas = async (stateErrorFunction) => {
+    try {
+
+      if(!boletimFile) throw new Error('SEM ARQUIVO!')
+
+      const RequestForm = new FormData();
+
+      RequestForm.append("boletim", {
+          name: boletimFile.name,
+          type: 'image/jpeg',
+          uri:
+          Platform.OS === "android" ? boletimFile.uri : boletimFile.uri.replace("file://", "")
+      });
+
+      const copyState = {...BOdatas}
+      copyState.tipo_registro = selectInputData[copyState.tipo_registro-1] 
+      copyState.data_ocorrido = copyState.data_ocorrido.toString() 
+
+      
+      Object.keys(copyState).forEach(key => {
+        RequestForm.append(key, copyState[key]);
+      });
+
+      Object.keys(locationDatas).forEach(key => {
+        RequestForm.append(key, locationDatas[key]);
+      });
+      
+
+      const response = await fetch(baseURL+'record/make_BO', {
+        method: 'POST',
+        headers:{authorization: authDatas.token},
+        body: RequestForm
+      })
+
+      setIsSuccess(response.status == 200)
+      setShowModal(false)
+    } catch (e) {
+      stateErrorFunction(e.message.includes('SEM ARQUIVO!'))
+      console.log("ERRO NO SUBMIT: [",e.message,"]")
+    }
+
+  }
+
+  const {setIsKeyboardOpen} = useContext(HideKeyboardContext)
+
+  const [showModal, setShowModal] = useState(false)
+  
+  const [showCalendar, setShowCalendar] = useState(false)
+
+  return loading ? (
+        <View style={{flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator color="#41BD1B" size="large"/>
+        </View>): 
+        (<KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding":"height"}>
+            <TouchableWithoutFeedback onPress={() => {
+              setShowCalendar(false)
+              Keyboard.dismiss()  
+            }}>{
+              isSuccess ? 
+              <View style={{height: '100%'}}>
+                <Header 
+                    navigation={navigation} 
+                    page="Verificar Segurança"/>
+                <WrapperSuccessImage>
+                  <SuccessText>Seu registro está em análise :{')\n'}Por favor, aguarde para inserir outro</SuccessText>
+                  <SuccessImage source={require('../../assets/RegisterBOimgs/CompletedInAnalyze.png')}/>
+                </WrapperSuccessImage>
+              </View>
+              :
+                <Container>
+                  <IconRegistry icons={EvaIconsPack}/>
+                  <Header 
+                    navigation={navigation} 
+                    page="Verificar Segurança"/>
+                  <PageTitle>
+                    <Text 
+                      style={{
+                        fontFamily:'RobotoLight', 
+                        fontSize:25, 
+                        textAlign: 'center'}}>
+                      {nextPage == 1 ? "PREENCHA OS CAMPOS PARA ENVIAR O BOLETIM" : "SÓ MAIS ALGUMAS INFORMAÇÕES"}
+                    </Text>
+                    <Ionicons name="ios-arrow-down" size={30} color="black" />
+                  </PageTitle>
+                  {nextPage==1 ? (
+                    <FirstPage 
+                      {...BOdatas}
+                      loadingState={loadingButton}
+                      Changed={StateValueHandler}
+                      ButtonPressed={NextPage}
+                    />
+                  ):(
+                    <SecondPage
+                      {...BOdatas}  
+                      boletimFile = {boletimFile}
+                      selectInputData={selectInputData}
+                      onSelectedDatePicker={StateValueHandler}
+                      onSelectedSelectType={StateValueHandler}
+                      selectPDF={selectPDF}
+                      showCalendar={showCalendar} 
+                      setShowCalendar={setShowCalendar}
+                      setShowModal={setShowModal}
+                      ButtonPressed={()=> setNextPage(1)}
+                    />
+                  )}
                   
-                  labelText="Identificação"
-
-                  maxLength={50}
-                  placeholder="CEP, nome (bairro ou rua)"
-                />
-            </InputContainer>
-
-            <InputContainer>
-              <HeaderInputContainer>
-                <Ionicons name="md-paper" size={28} color="black" />
-                <TitleInputContainer>Sobre o ocorrido</TitleInputContainer>
-              </HeaderInputContainer>
-              <AppInputBox
-                  onChangeText={description => StateValueHandler("description", description)}
-                  value={BOdatas.description}
-                  
-                  labelText="Data"
-
-                  placeholder="Quando ocorreu o incidente?"
-
-                  keyboardType="decimal-pad"
-                  maxLength={11}
-              />
-              <AppInputBox
-                onChangeText={dateOccurred => StateValueHandler("dateOccurred", dateOccurred)}
-                value={BOdatas.dateOccurred}
-                
-                labelText="Descrição"
-
-                multiline = {true}
-                maxLength={150}
-                style={{textAlignVertical: 'top'}}
-                placeholder="Descreva o que ocorreu"
-              />
-            </InputContainer>
-          </KeyboardAvoidingView>
-        </TouchableWithoutFeedback>
-      </Container>
+                </Container>
+            }
+            </TouchableWithoutFeedback>
+            
+            {
+                showModal && <ConfirmModal 
+                                {...BOdatas} 
+                                tipo_registro={selectInputData[BOdatas.tipo_registro-1]} 
+                                hideModal={() =>{ 
+                                  setIsKeyboardOpen(false)
+                                  setShowModal(false)
+                                }}
+                                onSubmit = {SubmitDatas}
+                                />
+            }
+        </KeyboardAvoidingView>
   );
 }
 
-export default registerBO;
+export default RegisterBO;
--- mobile/src/screens/registerBO/styles.js
+++ mobile/src/screens/registerBO/styles.js
@@ -1,8 +1,12 @@
-import { Entypo, Ionicons } from '@expo/vector-icons'
+import { Calendar, Layout } from '@ui-kitten/components'
+import { Dimensions } from 'react-native'
 import styled, { css } from 'styled-components'
 
-export const Container = styled.ScrollView`
+export const Container = styled.View` 
+    position: relative;
+
     background-color: white;
+    height: 100%;
 `
 
 export const PageTitle = styled.View`
@@ -10,8 +14,32 @@ export const PageTitle = styled.View`
     padding: 30px;
 `
 
+export const WrapperSuccessImage = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;    
+    background-color: white;
+
+    margin-bottom: 60px;
+`
+
+export const SuccessText = styled.Text`
+    font-family: 'RobotoLight';
+    font-size: 22px;
+    text-align: center;
+
+    padding: 0px 10px;
+`
+
+export const SuccessImage = styled.Image`
+    height: ${`${Dimensions.get('screen').height/2.5}px`};
+    width: ${`${Dimensions.get('screen').height/2.5}px`};
+`
+
 export const InputContainer = styled.View`
-    margin: 30px 0px;
+    flex: 1;
+    margin: 20px 0px;
+    margin-bottom: 60px;
 `
 
 export const HeaderInputContainer = styled.View`
@@ -19,7 +47,7 @@ export const HeaderInputContainer = styled.View`
     flex-direction: row;
     align-items: center;
 
-    margin: 0px 0px 5px 30px;
+    margin: 0px 0px 20px 30px;
 
     border-bottom-width: 1px;
     border-bottom-color: whitesmoke;
@@ -31,4 +59,95 @@ export const TitleInputContainer = styled.Text`
 
     font-family: 'RobotoMedium';
     font-size: 25px; 
+`
+
+export const PickerContainer = styled.View`
+    border: 1px solid gainsboro;
+    background-color: white;
+    border-radius: 3;
+
+    margin: 15px 30px;
+    height: 50px;
+`
+
+export const PickerFile = styled.TouchableOpacity`
+    margin: 10px 30px;
+    margin-bottom: 25px;
+`
+
+export const WrapperContentPickerFile  = styled.View`
+    align-items: center;
+
+    border-bottom-width: 1;
+    border-bottom-color: ${props => props.isExistError ? "#DC3545" : "gainsboro"};
+
+    padding: 5px;
+`
+
+export const TextPickerFile = styled.Text`
+    font-family: 'RobotoLight';
+    font-size: 14px;
+    color: ${props => props.isExistError ? "#DC3545" : "gray"} ; 
+    text-align: center;
+`
+
+export const ButtonShowCalendar = styled.View`
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+
+    margin: 0px 30px;
+    height: 50px;
+
+    background-color: rgb(248,249,253);
+    border-radius: 4px;
+    border: 1px solid rgb(237,238,242);
+
+    padding: 0px 15px;
+`
+
+export const ButtonShowCalendarText = styled.Text`
+    font-size: 16px;
+    color: rgb(141,156,179);
+`
+
+export const CalendarBlock = styled(Calendar)`
+    background-color: white;
+    
+    margin: 20px 30px;
+
+    position: absolute;
+    z-index: 4;
+
+    display: flex;
+
+    
+`
+
+export const DropShadowCalendar = styled.TouchableWithoutFeedback`
+    ${props => !props.show && css`display: none;`}
+`
+
+export const WrapperButton = styled.View`
+    justify-content: flex-start;
+
+    margin-bottom: 30px;
+`
+
+export const ButtonCalendarLabel = styled.Text`
+    font-size: 12px;
+    color: rgb(141,156,179);
+
+    background-color: white;
+
+    margin: 5px 30px;
+`
+
+export const Error = styled.Text`
+    font-family: 'RobotoLight';
+    font-size: 12px;
+    color: #DC3545;
+
+    margin-left: 30px;
+    margin-top: -8px;
 `
